053fa24c6f621ba49da82d60b1d1d5fae620ae0b
Yann Le Doaré <yann@linuxconsole.org>
2023-04-24 15:00:30 +0000

    add docker-compose to run.sh

--- dolibarr/run.sh
+++ dolibarr/run.sh
@@ -1,2 +1,4 @@
 [ ! -e 17.0.1.tar.gz ] && wget https://github.com/Dolibarr/dolibarr/archive/refs/tags/17.0.1.tar.gz
 [ ! -e dolibarr-17.0.1 ] && tar xzvf 17.0.1.tar.gz
+cd build/docker
+HOST_USER_ID=$(id -u) docker-compose up -d
