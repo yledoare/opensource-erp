9ad28b8706c63fd1fa2cc0e613e72b13bd5e1e2b
Yann Le Doaré <yann@linuxconsole.org>
2023-06-07 09:45:42 +0000

    update DOLIBARR=17.0.2

--- dolibarr/run.sh
+++ dolibarr/run.sh
@@ -1,4 +1,5 @@
-[ ! -e 17.0.1.tar.gz ] && wget https://github.com/Dolibarr/dolibarr/archive/refs/tags/17.0.1.tar.gz
-[ ! -e dolibarr-17.0.1 ] && tar xzvf 17.0.1.tar.gz
+DOLIBARR=17.0.2
+[ ! -e $DOLIBARR.tar.gz ] && wget https://github.com/Dolibarr/dolibarr/archive/refs/tags/$DOLIBARR.tar.gz
+[ ! -e dolibarr-$DOLIBARR ] && tar xzvf $DOLIBARR.tar.gz
 cd build/docker
 HOST_USER_ID=$(id -u) docker-compose up -d
