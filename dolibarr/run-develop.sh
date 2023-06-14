c64742c3af990f6505fe08569801bc2f811690f6
Yann Le Doaré <yann@linuxconsole.org>
2023-06-14 14:28:02 +0000

    Docker images from https://github.com/yledoare/docker-images

--- dolibarr/run-develop.sh
+++ dolibarr/run-develop.sh
@@ -1,5 +1,5 @@
-[ ! -e dolibarr-git ] && git clone --depth 1  https://github.com/Dolibarr/dolibarr dolibarr-git
-cd dolibarr-git/build/docker
+[ ! -e dolibarr-develop ] && git clone --depth 1  https://github.com/Dolibarr/dolibarr dolibarr-develop
+cd dolibarr-develop/build/docker
 git checkout docker-compose.yml
 sed -i 's/3306:3306/3307:3306/' docker-compose.yml
 sed -i 's/8080:80/8089:80/' docker-compose.yml
@@ -8,8 +8,9 @@ sed -i 's/9000:9000/9001:9001/' docker-compose.yml
 sed -i 's/25:25/2525:2525/' docker-compose.yml
 sed -i 's/host-gateway/127.0.0.1/' docker-compose.yml
 sed -i 's/rootpassfordev/rootpwd/' docker-compose.yml
+sed -i 's/build: ./image: dolibarr-develop/' docker-compose.yml
 HOST_USER_ID=$(id -u) docker-compose up -d
 # docker exec -ti docker_webprd_1 mysql -h mariadbprd -u root -prootpwd dolibarr
-# docker cp dolibarr-git/dev/initdemo/mysqldump_dolibarr_17.0.0.sql docker_web_1:.
+# docker cp dolibarr-develop/dev/initdemo/mysqldump_dolibarr_17.0.0.sql docker_web_1:.
 # docker exec -ti docker_web_1 bash
 # mysql -h mariadb -u root -prootpwd dolibarr < /mysqldump_dolibarr_17.0.0.sql
