1ffc23664408b5927731bfad46097c3c42834c2f
Yann Le Doaré <yann@linuxconsole.org>
2023-06-23 21:30:40 +0000

    Odoo-v16 Ok

--- odoo-v16/run.sh
+++ odoo-v16/run.sh
@@ -10,6 +10,7 @@ do
   git checkout origin/$ODOO || exit 2
   ls | while read module
   do
+	  [ "$module" = "requirements.txt" ] && cat requirements.txt >> ../oca/requirements.txt
 	  test -f $module && continue
 	  # [ "$module" = "setup" ] && continue
 	  echo "Module $module"
@@ -22,7 +23,11 @@ docker-compose up -d
 
 #4G RAM
 sleep 10
-docker exec -u root -ti odoo-v16_web_1 sed -i 's/limit_time_real = 120/limit_time_real = 320/' /etc/odoo/odoo.conf
+docker exec -u root -ti odoo-v16_web_1 /bin/sh -c "echo limit_time_real = 320 >> /etc/odoo/odoo.conf"
+docker exec -u root -ti odoo-v16_web_1 pip3 install -r /mnt/extra-addons/requirements.txt 
+docker exec -u root -ti odoo-v16_web_1 pip3 install python-stdnum==1.18
+#docker exec -u root -ti odoo-v16_web_1 pip3 install python-stdnum==1.18 unidecode pgpy astor
+#docker exec -u root -ti odoo-v16_web_1 sed -i 's/limit_time_real = 120/limit_time_real = 320/' /etc/odoo/odoo.conf
 #docker exec -u root -ti odoo-v16_web_1 /bin/sh -c "echo limit_memory_hard = 3684354560 >> /etc/odoo/odoo.conf"
 #docker exec -u root -ti odoo-v16_web_1 /bin/sh -c "echo limit_memory_soft = 3184354560 >> /etc/odoo/odoo.conf"
 #docker exec -u root -ti odoo-v16_web_1 /bin/sh -c "echo list_db = False >> /etc/odoo/odoo.conf"
