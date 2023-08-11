ace819b2d3b2f79afec8af51a510dc3fcfc7b30f
Yann Le Doaré <yann@linuxconsole.org>
2023-08-11 08:16:03 +0000

    update dolibarr to 17.0.3

--- dolibarr-17/run.sh
+++ dolibarr-17/run.sh
@@ -1,4 +1,4 @@
-DOLIBARR=17.0.2
+DOLIBARR=17.0.3
 [ ! -e $DOLIBARR.tar.gz ] && wget https://github.com/Dolibarr/dolibarr/archive/refs/tags/$DOLIBARR.tar.gz
 [ ! -e dolibarr-$DOLIBARR ] && tar xzvf $DOLIBARR.tar.gz
 cd build/docker
