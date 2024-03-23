ab8c0687b85a2156ae029d3590b52493c4301a1e
Yann Le Doaré <yann@linuxconsole.org>
2024-03-23 08:00:22 +0000

    Add odoo-v16-lefilament

--- dolibarr-18/run.sh
+++ dolibarr-18/run.sh
@@ -1,4 +1,4 @@
-DOLIBARR=18.0.1
+DOLIBARR=18.0.5
 [ ! -e $DOLIBARR.tar.gz ] && wget https://github.com/Dolibarr/dolibarr/archive/refs/tags/$DOLIBARR.tar.gz
 [ ! -e dolibarr-$DOLIBARR ] && tar xzvf $DOLIBARR.tar.gz
 cd dolibarr-$DOLIBARR 
