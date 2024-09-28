c9e514c4e0df61a1b5a9b1da01160beaa978411e
Yann Le Doaré <yann@linuxconsole.org>
2024-09-28 15:31:34 +0000

    fix odoo v16

--- odoo-v16/run.sh
+++ odoo-v16/run.sh
@@ -23,7 +23,7 @@ docker-compose up -d
 
 #4G RAM
 sleep 10
-docker exec -u root -ti odoo-v16_web_1 /bin/sh -c "echo limit_time_real = 320 >> /etc/odoo/odoo.conf"
+# docker exec -u root -ti odoo-v16_web_1 /bin/sh -c "echo limit_time_real = 320 >> /etc/odoo/odoo.conf"
 
 docker exec -u root -ti odoo-v16_web_1 pip3 install -r /mnt/extra-addons/requirements.txt 
 docker exec -u root -ti odoo-v16_web_1 pip3 install python-stdnum==1.18
