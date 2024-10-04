0d18b628bbf5362872ed6b64d700187a52479b37
Yann Le Doaré <yann@linuxconsole.org>
2024-10-04 06:52:44 +0000

    Update odoo-v17

--- odoo-v17/run.sh
+++ odoo-v17/run.sh
@@ -3,11 +3,10 @@ ODOO="17.0"
 install -d addons/oca
 install -d filestore
 
-for oca in account-invoicing bank-payment community-data-files edi l10n-france reporting-engine intrastat-extrastat e-commerce  geospatial  manufacture  server-tools
+for oca in server-env account-invoicing helpdesk hr bank-payment community-data-files edi l10n-france reporting-engine intrastat-extrastat e-commerce  geospatial  manufacture  server-tools
 do
-  [ ! -e addons/$oca ] && git clone https://github.com/OCA/$oca.git addons/$oca
+  [ ! -e addons/$oca ] && git clone --single-branch --branch "${ODOO}" --depth 1 https://github.com/OCA/$oca.git addons/$oca
   cd addons/$oca || exit 1
-  git checkout origin/$ODOO || exit 2
   ls | while read module
   do
 	  [ "$module" = "requirements.txt" ] && cat requirements.txt >> ../oca/requirements.txt
@@ -23,11 +22,12 @@ docker-compose up -d
 
 #4G RAM
 sleep 10
-docker exec -u root -ti odoo-v17_web_1 /bin/sh -c "echo limit_time_real = 320 >> /etc/odoo/odoo.conf"
+#docker-compose exec -u root web /bin/sh -c "echo limit_time_real = 320 >> /etc/odoo/odoo.conf"
 
-docker exec -u root -ti odoo-v17_web_1 pip3 install -r /mnt/extra-addons/requirements.txt 
-docker exec -u root -ti odoo-v17_web_1 pip3 install python-stdnum==1.18
+docker-compose exec -u root web pip3 install -r /mnt/extra-addons/requirements.txt 
+#docker-compose exec -u root web pip3 install python-stdnum==1.18
 
-docker exec -u root -ti odoo-v17_web_1 apt-get -y update
-docker exec -u root -ti odoo-v17_web_1 apt-get -y install procps vim
-docker restart odoo-v17_web_1
+docker-compose exec -u root web apt-get -y update
+docker-compose exec -u root web apt-get -y install procps vim
+
+docker-compose restart web
