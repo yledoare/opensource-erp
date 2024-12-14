66d2b4df533ab644f0ca0510845b1add3fb21190
Yann Le Doaré <yann@linuxconsole.org>
2024-12-14 16:23:34 +0000

    Add odoo-v18

---
+++ odoo-v18/get-oca-modules.sh
@@ -0,0 +1,33 @@
+ODOO="18.0"
+
+install -d addons-oca
+install -d filestore
+
+for oca in server-env account-invoicing helpdesk hr bank-payment community-data-files edi l10n-france reporting-engine intrastat-extrastat e-commerce  geospatial  manufacture  server-tools
+do
+  [ ! -e addons-oca/$oca ] && git clone --single-branch --branch "${ODOO}" --depth 1 https://github.com/OCA/$oca.git addons-oca/$oca
+  cd addons-oca/$oca || exit 1
+  ls | while read module
+  do
+	  [ "$module" = "requirements.txt" ] && cat requirements.txt >> ../oca/requirements.txt
+	  test -f $module && continue
+	  # [ "$module" = "setup" ] && continue
+	  echo "Module $module"
+	  cp -fR $module ../oca
+  done 
+  cd ../..
+done
+
+docker-compose up -d
+
+#4G RAM
+sleep 10
+#docker-compose exec -u root web /bin/sh -c "echo limit_time_real = 320 >> /etc/odoo/odoo.conf"
+
+docker-compose exec -u root web pip3 install -r /mnt/extra-addons/requirements.txt 
+#docker-compose exec -u root web pip3 install python-stdnum==1.18
+
+docker-compose exec -u root web apt-get -y update
+docker-compose exec -u root web apt-get -y install procps vim
+
+docker-compose restart web
---
+++ odoo-v18/update.sh
@@ -0,0 +1,22 @@
+ODOO="17.0"
+
+install -d addons/oca
+install -d filestore
+
+for oca in account-invoicing server-env
+do
+  [ ! -e addons/$oca ] && git clone https://github.com/OCA/$oca.git addons/$oca
+  cd addons/$oca || exit 1
+  git checkout origin/$ODOO || exit 2
+  ls | while read module
+  do
+	  [ "$module" = "requirements.txt" ] && cat requirements.txt >> ../oca/requirements.txt
+	  test -f $module && continue
+	  # [ "$module" = "setup" ] && continue
+	  echo "Module $module"
+	  cp -fR $module ../oca
+  done 
+  cd ../..
+done
+
+docker restart odoo-v17_web_1
